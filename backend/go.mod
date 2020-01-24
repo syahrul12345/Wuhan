43e2c8d0e3b0452613801202439d66adc842d92d
syahrul12345 <muhdsyahrulnizam123@gmail.com>
2020-01-24 23:19:32 +0800

    Daemonize

--- backend/go.mod
+++ backend/go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/gorilla/mux v1.7.3
 	github.com/rs/cors v1.7.0
+	github.com/sevlyar/go-daemon v0.1.5
 	github.com/syndtr/goleveldb v1.0.0
 )
--- backend/main.go
+++ backend/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"backend/controller"
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/sevlyar/go-daemon"
 )
 
 const (
@@ -11,6 +14,28 @@ const (
 )
 
 func main() {
+	cntxt := &daemon.Context{
+		PidFileName: "sample.pid",
+		PidFilePerm: 0644,
+		LogFileName: "sample.log",
+		LogFilePerm: 0640,
+		WorkDir:     "./",
+		Umask:       027,
+		Args:        []string{"[go-daemon sample]"},
+	}
+
+	d, err := cntxt.Reborn()
+	if err != nil {
+		log.Fatal("Unable to run: ", err)
+	}
+	if d != nil {
+		return
+	}
+
+	defer cntxt.Release()
+
+	log.Print("- - - - - - - - - - - - - - -")
+	log.Print("daemon started")
 	start()
 }
 
